6e8179e25b7fdad2f2b5292b4e5d755438f9d155
blanchejose <158100912+blanchejose@users.noreply.github.com>
2025-10-29 17:19:14 -0400

    feat:implements collider repository

---
+++ src/Doctrine/CollidableRepository.java
@@ -0,0 +1,42 @@
+package Doctrine;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class CollidableRepository implements Iterable<StaticEntity> {
+   //  tableau de tous les enfants qui peuvent entrer en collission
+    private static CollidableRepository instance;
+    private final List<StaticEntity> registeredEntities;
+
+    public static CollidableRepository getInstance() {
+        if (instance == null) {
+            instance = new CollidableRepository();
+        }
+        return instance;
+    }
+
+    public void registerEntity(StaticEntity entity) {
+        registeredEntities.add(entity);
+    }
+    public void unregisterEntity(StaticEntity entity) {
+        registeredEntities.remove(entity);
+    }
+    public void RegisteredEntities(Collection<StaticEntity> entities) {
+        registeredEntities.addAll(entities);
+    }
+    public  void unregisterEntities(Collection<StaticEntity> entities) {
+        registeredEntities.removeAll(entities);
+    }
+    public int count(){
+        return registeredEntities.size();
+    }
+    @Override
+    public Iterator<StaticEntity> iterator() {
+        return registeredEntities.iterator();
+    }
+    private CollidableRepository() {
+        registeredEntities = new ArrayList<>();
+    }
+}
